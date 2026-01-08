ea612946a5b24340b6b08be4a235e36aa7f7ffb5
karimim <muhammad.karimi@post.ch>
2026-01-08 09:09:34 +0100

    feat: add basic style

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
 import { defineCollection, z } from "astro:content";
 
 export const collections = {
-  blog: defineCollection({
+  blogs: defineCollection({
     schema: z.object({
       title: z.string(),
       description: z.string(),
       pubDate: z.date(),
       author: z.string(),
       tags: z.array(z.string()),
-      draft: z.string().default("false"),
+      draft: z.boolean().default(false),
       slug: z.string().optional(),
     }),
   }),
